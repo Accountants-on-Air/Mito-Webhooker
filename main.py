ae4658e094988c62265587925839daee4f1d06e1
Ahmed Métwalli <54792909+metwallusion@users.noreply.github.com>
2023-12-20 20:18:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-import json
+from executer import executer
 app = FastAPI()
 @app.post("/webhook")
 async def root(request: Request):
     data = await request.json()
     print(data)
+    executer(data)
     return JSONResponse(content=data)
