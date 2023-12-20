b0a06a162dcf6e419e244d26fcc3a3a685fe7b5a
Ahmed Métwalli <54792909+metwallusion@users.noreply.github.com>
2023-12-20 18:45:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from monday import MondayClient
+import json
 app = FastAPI()
-
+client = MondayClient('eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIyMDI0MjExNSwiYWFpIjoxMSwidWlkIjozNzc5MjU0MCwiaWFkIjoiMjAyMy0wMS0xMFQxNjowNzoxNS4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6NjkwOTQ3NCwicmduIjoidXNlMSJ9.ZiE5WtSS3eW5NgNLoOO7jHrDZvRBHMmmNApWhnfRIhk')
+board_id='5015701381'
+async def func(board_id,id_,query):
+    client.items.change_multiple_column_values(board_id,id_,query)
 @app.post("/webhook")
 async def root(request: Request):
     data = await request.json()
-    print(data)
+    target_id = data['event']['pulseId']
+    id_ = data['event']['value']['linkedPulseIds'][0]['linkedPulseId']
+    query = json.loads('{{\"connect_boards1\" : {{\"item_ids\" : {}}}}}'.format(target_id))
+    await func(board_id,id_,query)
     return JSONResponse(content=data)
