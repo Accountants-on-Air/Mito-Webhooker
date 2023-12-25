1de8a78afc55de5c8e7c88c3e2a00a6c8c5acf43
ahmedcmpunkx@gmail.com <ametwali@qmkconsulting.com>
2023-12-25 12:08:19 +0200

    Added tax prep

--- executer.py
+++ executer.py
@@ -1,5 +1,9 @@
 from monday import MondayClient
 import json
 client = MondayClient('eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIyMDI0MjExNSwiYWFpIjoxMSwidWlkIjozNzc5MjU0MCwiaWFkIjoiMjAyMy0wMS0xMFQxNjowNzoxNS4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6NjkwOTQ3NCwicmduIjoidXNlMSJ9.ZiE5WtSS3eW5NgNLoOO7jHrDZvRBHMmmNApWhnfRIhk')
-def executer(data):
+def executer(data): #Master Database
     client.items.change_multiple_column_values(5015701381,data['event']['value']['linkedPulseIds'][0]['linkedPulseId'],json.loads('{{\"connect_boards1\" : {{\"item_ids\" : {}}}}}'.format([data['event']['pulseId']])))
+
+def executer2(data): #Tax Prep Assignments
+    #client.items.change_multiple_column_values(1604758265,)
+    print(data)
--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from executer import executer
 app = FastAPI()
-@app.post("/webhook")
+@app.post("/webhook") #Master Database
 async def root(request: Request):
     data = await request.json()
     print(data)
     executer(data)
+    return JSONResponse(content=data)
+
+@app.post("/webhook2")
+async def root(request: Request):
+    data = await request.json()
+    print(data)
     return JSONResponse(content=data)
